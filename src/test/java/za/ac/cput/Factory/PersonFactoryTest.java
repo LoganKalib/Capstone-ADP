45f459ca4a19328b088f179f3797c60d972972dd
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-03-21 11:16:12 +0200

    Create PersonFactoryTest.java

---
+++ src/test/java/za/ac/cput/Factory/PersonFactoryTest.java
@@ -0,0 +1,21 @@
+package za.ac.cput.Factory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void buildPerson() {
+    }
+
+    @Test
+    void testBuildPerson() {
+    }
+}
